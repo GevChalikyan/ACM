5a0309da4c2c0d1fefc8c667f1451bed83a8a011
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-19 08:50:31 -0700

    Initial Commit

---
+++ 1 - Data Science & Machine Learning/project.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+df = pd.read_csv("data.csv")
+
+
+def main():
+  
+  if df['Num_Bedrooms'].isnull().any():
+    df.dropna(subset=['Num_Bedrooms'], inplace=True)
+
+
+
+  mask = df['Price_USD'].apply(lambda x: isinstance(x, str))
+  df.loc[mask, 'Price_USD'] = df.loc[mask, 'Price_USD'].str.replace('$', '')
+  df.loc[mask, 'Price_USD'] = df.loc[mask, 'Price_USD'].str.replace(',', '')
+  df.loc[mask, 'Price_USD'] = df.loc[mask, 'Price_USD'].astype(int)
+
+  if df['Price_USD'].isnull().any():
+    mean_price = df['Price_USD'].mean() 
+    df['Price_USD'] = df['Price_USD'].fillna(mean_price)
+    
+
+
+  print(df)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
