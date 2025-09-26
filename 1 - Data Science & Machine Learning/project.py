83cb7f1fa2c8331dc48a82f166735304165e2330
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-26 13:42:52 -0700

    Simplified Project and Added Guidelines for a Template

--- 1 - Data Science & Machine Learning/project.py
+++ 1 - Data Science & Machine Learning/project.py
@@ -1,34 +1,32 @@
 import pandas as pd
 
-df = pd.read_csv("../data/data.csv")
+df = pd.read_csv('../data/data.csv')
 
 
 def main():
+
+  # Print the dataframe to look for missing values or invalid data
+  print('Dataframe before cleaning:')
+  print(df)
+  print('\n')
   
-  if df['Num_Bedrooms'].isnull().any():
-    df.dropna(subset=['Num_Bedrooms'], inplace=True)
-
-
-
-  mask = df['Price_USD'].apply(lambda x: isinstance(x, str))
-  df.loc[mask, 'Price_USD'] = df.loc[mask, 'Price_USD'].str.replace('$', '')
-  df.loc[mask, 'Price_USD'] = df.loc[mask, 'Price_USD'].str.replace(',', '')
-
+  # Drop all rows with missing values or invalid data
   df['Price_USD'] = pd.to_numeric(df['Price_USD'], errors='coerce')
-
-
-
-  if df['Price_USD'].isnull().any():
-    mean_price = df['Price_USD'].mean() 
-    df['Price_USD'] = df['Price_USD'].fillna(mean_price)
+  df.dropna(inplace=True)
     
 
 
+  # Print the cleaned dataframe to verify the the data is clean
+  print('Dataframe after cleaning:')
   print(df)
+  print('\n')
 
 
 
 
 
 if __name__ == "__main__":
+  
+  # Make a couple of new lines for readability and run main
+  print('\n')
   main()
