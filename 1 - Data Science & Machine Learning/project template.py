6ae42e91e8d260d0251df1c0485382265a2d2f4c
Gev Chalikyan <72428068+GevChalikyan@users.noreply.github.com>
2025-09-26 13:50:09 -0700

    Added Template File for Guidance

---
+++ 1 - Data Science & Machine Learning/project template.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+data_file_path = # *** PATH TO YOUR DATA FILE AS A STRING ***
+df = pd.read_csv(data_file_path)
+
+
+def main():
+
+  # 1) Print the dataframe to look for missing values or invalid data
+
+  # ******************************************************
+
+
+
+  # 2) Drop all rows with missing values or invalid data
+    
+  # ******************************************************
+
+
+
+  # 3) Print the cleaned dataframe to verify the the data is clean
+
+  # ******************************************************
+
+  return
+
+
+
+
+
+if __name__ == "__main__":
+  
+  main()
